eb01192a769ae3b3d3477fdc893ae8b920cc0e0c
sharkbilla07-hue <sharkbilla07@gmail.com>
2026-02-19 02:11:22 +0530

    Delete analyzers directory

--- analyzers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Analyzers module
-from .pagespeed import run_pagespeed_audit, fetch_pagespeed_data, parse_pagespeed_results
-from .seo_analyzer import SEOAnalyzer, run_seo_analysis
-from .technical_seo import TechnicalSEOAnalyzer, run_technical_seo_analysis
-from .content_analyzer import ContentAnalyzer, run_content_analysis
-from .security_analyzer import SecurityAnalyzer, run_security_analysis
-from .ai_recommendations import get_ai_recommendations
